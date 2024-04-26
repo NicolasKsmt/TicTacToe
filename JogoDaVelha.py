f906927f3c20083a82bcdf718cacb50c8ce018e4
Nicolas Machado <113930152+NicolasKsmt@users.noreply.github.com>
2024-04-26 17:53:41 -0300

    Add files via upload

---
+++ JogoDaVelha.py
@@ -0,0 +1,15 @@
+import os
+import random
+
+playAgain = "s"
+plays = 0
+playerToPlay = 2
+maxPlays = 9
+gameWin = "n"
+# the blank spaces are meant to be filled with the player's symbol to preserve the format
+ticTacToe = [
+    [" ", " ", " ",],
+    [" ", " ", " ",],
+    [" ", " ", " "]
+]
+
